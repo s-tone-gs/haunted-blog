0f6e7fa4ef6683abde22aae7d3c5aaf02408b701
shoma <s.tone@genbasupport.com>
2026-01-22 15:53:32 +0900

    fix: 投稿者以外でも秘密のブログにアクセスできるバグを修正

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -10,7 +10,11 @@ class BlogsController < ApplicationController
     @blogs = Blog.search(params[:term]).published.default_order
   end
 
-  def show; end
+  def show
+    return if !@blog.secret || @blog.owned_by?(current_user)
+
+    render status: :not_found, html: helpers.tag.strong('他ユーザーの秘密のブログは見れません')
+  end
 
   def new
     @blog = Blog.new
